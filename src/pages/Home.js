c2a1b18cb0bd1fe8e0a074e4b9335657940fc992
Emmanuel-EEzekiel <emamex44@gmail.com>
2021-12-28 14:28:01 +0100

    add file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,25 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-            welcome Home
-        </div>
+        <>
+     <header>
+        <nav>
+            <div> 
+                <h1>Beach Hotel</h1>
+            </div>
+
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+
+            <div>
+
+            </div>
+         </nav>     
+    </header>   
+        </>
     )
 }
 
